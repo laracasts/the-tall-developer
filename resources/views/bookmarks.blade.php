b1e1ddc04010f5ba3dd96a919eb570aeb7103869
Shruti Balasa <shrutibalasa@Shrutis-MacBook-Pro.local>
2025-07-16 11:30:45 +0530

    Episode 8 - Add authentication, routes and views

---
+++ resources/views/bookmarks.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+    <main
+            x-data="{showToast: false, message: ''}"
+            class="p-8"
+        >
+            <h1 class="font-bold text-4xl text-amber-600">Bookmarks</h1>
+            <p class="mt-1 text-zinc-500">These are your favorite roles</p>
+    </main>
+</x-layouts.app>
